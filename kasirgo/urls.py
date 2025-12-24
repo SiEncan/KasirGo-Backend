787b62499b08f58a31bee3fe288ab8f17cef1c5d
Adnan Alvito <adnanalvitomustaqim@gmail.com>
2025-12-24 20:51:09 +0700

    feat: Set up a welcome endpoint.

--- kasirgo/urls.py
+++ kasirgo/urls.py
@@ -19,7 +19,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "Welcome to KasirGo Backend API!", "status": "running"})
+
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
